ff04747f1a0a5b0b97410108742dea79dab4efdd
Josh Allen <joshmrallen@gmail.com>
2020-02-29 04:23:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,7 +37,7 @@ def find_the_cheese(say_cheese)
   index = 0
   while index < cheese_types.length do 
     if say_cheese.include?(cheese_types[index])
-      puts cheese_types[index]
+      p cheese_types[index]
       return true
     end
     index += 1
