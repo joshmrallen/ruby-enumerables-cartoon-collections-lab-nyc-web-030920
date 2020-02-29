2492f9b39582de814000d383e403ca860ff160f8
Josh Allen <joshmrallen@gmail.com>
2020-02-29 04:22:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,8 +36,9 @@ def find_the_cheese(say_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   index = 0
   while index < cheese_types.length do 
-    if say_cheese.any?{|word| word == cheese_types[index]}
+    if say_cheese.include?(cheese_types[index])
       puts cheese_types[index]
+      return true
     end
     index += 1
   end
